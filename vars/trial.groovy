276db52794a9866dde47b5bfd092ddc62ac08266
AnanditaSahu <101005280+AnanditaSahu@users.noreply.github.com>
2022-03-05 22:55:31 +0530

    Update trial.groovy

--- vars/trial.groovy
+++ vars/trial.groovy
@@ -1,5 +1,11 @@
+import groovy.io.FileType
+
 def call(name){
   echo "Name is ${name}"
-  ls
-  pwd
+  def list = []
+
+  def dir = new File("path_to_parent_dir")
+  dir.eachFileRecurse (FileType.FILES) { file ->
+    list << file
+  }
 }
