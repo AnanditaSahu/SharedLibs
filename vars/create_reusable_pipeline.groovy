d7893b72d63e8625293a28c4e6ea866a3dcf20f8
AnanditaSahu <101005280+AnanditaSahu@users.noreply.github.com>
2022-03-05 23:21:40 +0530

    Update create_reusable_pipeline.groovy

--- vars/create_reusable_pipeline.groovy
+++ vars/create_reusable_pipeline.groovy
@@ -7,20 +7,17 @@ def call(Map templateParams) {
     pipeline {
         agent any
         parameters {
-            string(name: 'inputParam', description: 'Some pipeline parameters')
+            string(name: 'input2', default: 10)
         }
         stages {
-            stage('Stage one') {
-                steps {
-                    script {
-                        echo "Parameter passed to pipeline: " + templateParams.someParam
-                    }
-                }
-            }
-            stage('Stage two') {
-                steps {
-                    script {
-                        echo "Parameter defined in pipeline: " + params.inputParam
+            stage('Demo'){
+                steps{
+                    trial("Anandita Sahu")
+                    script{
+                        calculator.add(templateParams.input1, params.input2)
+                        calculator.sub(templateParams.input1, params.input2)
+                        calculator.mul(templateParams.input1, params.input2)
+                        calculator.div(templateParams.input1, params.input2)
                     }
                 }
             }
